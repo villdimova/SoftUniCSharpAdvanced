5f3b5356a76a4deb168fa874ad13e4ef0a40b85b
Velyana Dimova <70020646+villdimova@users.noreply.github.com>
2021-01-25 11:35:22 +0200

    Add files via upload

---
+++ Recursion/FactorielWithRecursion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FactorielWithRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal num = decimal.Parse(Console.ReadLine());
+           Console.WriteLine( Factorial(num));
+
+        }
+
+        static decimal Factorial(decimal num)
+        {
+            if (num==0)
+            {
+                return 1;
+            }
+            else
+            {
+                return num * Factorial(num - 1);
+            }
+        }
+    }
+}
