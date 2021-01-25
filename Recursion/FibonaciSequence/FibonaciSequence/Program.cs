d42550ea57d5f23c00f1eba26ed3e2742531eed2
Velyana Dimova <70020646+villdimova@users.noreply.github.com>
2021-01-25 22:13:51 +0200

    Add files via upload

---
+++ Recursion/FibonaciSequence/FibonaciSequence/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FibonaciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number:");
+            long n = long.Parse(Console.ReadLine());
+            Console.WriteLine(Fibunacci(n));
+        }
+
+        public static long Fibunacci(long n)
+        {
+            if (n<=1)
+            {
+                return 1;
+            }
+            return Fibunacci(n - 1) + Fibunacci(n - 2);
+        }
+    }
+}
